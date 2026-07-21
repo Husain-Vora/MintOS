2d38301a497ccf0a01b67c2e557dfa408316aef2
Husain-Vora <husain.vora.0192@gmail.com>
2026-07-21 21:05:29 +0530

    Programmable Interrupt Controller Implementation

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -1,12 +1,24 @@
 #include "idt.h"
 #include "config.h"
 #include "kernel.h"
-#include"memory/memory.h"
+#include "memory/memory.h"
+#include "io/io.h"
 
 struct idt_desc idt_descriptors[MINTOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
 extern void idt_load(struct idtr_desc* ptr);
+extern void int21h();
+extern void no_interrupt();
+
+void int21h_handler() {
+    print("keyboard pressed!!!\n");
+    outb( 0x20, 0x20 );
+}
+
+void no_interrupt_handler() {
+    outb(0x20, 0x20);
+}
 
 void idt_zero() {
     print( "Divide by zero error\n" );
@@ -26,8 +38,13 @@ void idt_init() {
     idtr_descriptor.limit = sizeof( idt_descriptors ) - 1;
     idtr_descriptor.base = (uint32_t) idt_descriptors;
 
-    idt_set(0, idt_zero);
+    for ( int i = 0; i < MINTOS_TOTAL_INTERRUPTS; i++ ) {
+        idt_set(i, no_interrupt);
+    }
+
+    idt_set( 0, idt_zero );
+    idt_set( 0x21, int21h );
 
     // load the interrupt descriptor table
-    idt_load(&idtr_descriptor);
+    idt_load( &idtr_descriptor );
 }
